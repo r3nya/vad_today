ba94b261050c1782424354e2003a6f771cdeb5a1
Andrew M <me@r3nya.ru>
2014-01-21 23:21:19 +0400

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VadToday::Application.config.secret_key_base = 'ffc0884c556285505050e9fda2a3718d8ae139f3d2e340ca3699f79cf7f6d435bb6c433b5086a8d2c61c7fe0efcf0b12418a46bfe460fae1904320e8aba184fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VadToday::Application.config.session_store :cookie_store, key: '_vad_today_session'
